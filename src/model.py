529cb7d810bfa1169c040d5fe11a4b919102075c
Guilherme Poleto <gpoleto@alunos.utfpr.edu.br>
2023-11-27 22:44:05 -0300

    updated model, train_model and readme

--- src/model.py
+++ src/model.py
@@ -2,32 +2,38 @@ import keras
 
 """
 This model is a convolutional neural network to classify different pets. Each pet is an individual pet, not a breed.
-There are 6 classes: dontcare, batima, botinha, linco, nico, peto.
+There are 5 classes: batima, botinha, linco, nico, peto.
 The images are JPEGs with 3 channels (RGB) 800x600, the model is trained on 40x30.
 """
+
 
 class Model(keras.Model):
     def __init__(self):
         super(Model, self).__init__()
-        self.model = keras.Sequential([
-            keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(40, 30, 3)),
-            keras.layers.MaxPooling2D((2, 2)),
-            keras.layers.Conv2D(64, (3, 3), activation='relu'),
-            keras.layers.MaxPooling2D((2, 2)),
-            keras.layers.Conv2D(64, (3, 3), activation='relu'),
-            keras.layers.MaxPooling2D((2, 2)),
-            keras.layers.Flatten(),
-            keras.layers.Dense(64, activation='relu'),
-            keras.layers.Dense(6)
-        ])
+        self.model = keras.Sequential(
+            [
+                keras.layers.Conv2D(
+                    64, (3, 3), activation="relu", input_shape=(40, 30, 3)
+                ),
+                keras.layers.MaxPooling2D((2, 2)),
+                keras.layers.Conv2D(32, (3, 3), activation="relu"),
+                keras.layers.MaxPooling2D((2, 2)),
+                keras.layers.Flatten(),
+                keras.layers.Dense(64, activation="relu"),
+                keras.layers.Dense(5, activation="softmax"),
+            ]
+        )
 
     def call(self, x):
         return self.model(x)
-    
+
     def predict(self, x):
         return self.call(x)
-    
-    def plot(self, filename='model.png'):
-        keras.utils.plot_model(self, to_file=filename, show_shapes=True, show_layer_names=True)
+
+    def plot(self, filename="model.png"):
+        keras.utils.plot_model(
+            self, to_file=filename, show_shapes=True, show_layer_names=True
+        )
+
 
 # Path: src/train_model.py
--- src/train_model.py
+++ src/train_model.py
@@ -3,38 +3,38 @@ from os import listdir
 
 import cv2
 import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 from model import Model, keras
 
+
 def main():
     parser = ArgumentParser()
-    parser.add_argument('--model', default='models/model.h5')
-    parser.add_argument('--dataset', default='data')
-    parser.add_argument('--epochs', type=int, default=10)
-    parser.add_argument('--batch_size', type=int, default=32)
-    parser.add_argument('--plot', default='models/model.png')
-    parser.add_argument('--classes', default='models/classes.txt')
-    parser.add_argument('--summary', default='models/summary.txt')
+    parser.add_argument("--model", default="models/model.h5")
+    parser.add_argument("--dataset", default="data")
+    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--plot", default="models/model.png")
+    parser.add_argument("--classes", default="models/classes.txt")
+    parser.add_argument("--summary", default="models/summary.txt")
+    parser.add_argument("--confusion_matrix", default="models/confusion_matrix.png")
     args = parser.parse_args()
 
     # convert each character in "poleto" to its ASCII code, then concatenate them and convert to int
-    seed = int(''.join([str(ord(c)) for c in 'poleto'])) % (2**32 - 1)
+    seed = int("".join([str(ord(c)) for c in "poleto"])) % (2**32 - 1)
 
     # load dataset
     train_data, validation_data = tf.keras.preprocessing.image_dataset_from_directory(
         args.dataset,
         validation_split=0.2,
-        subset="training",
+        subset="both",
         seed=seed,
         image_size=(40, 30),
-        batch_size=args.batch_size
-    ), tf.keras.preprocessing.image_dataset_from_directory(
-        args.dataset,
-        validation_split=0.2,
-        subset="validation",
-        seed=seed,
-        image_size=(40, 30),
-        batch_size=args.batch_size
+        batch_size=args.batch_size,
+        label_mode="categorical",
     )
 
     # create model
@@ -42,41 +42,52 @@ def main():
 
     # compile model
     model.compile(
-        optimizer='adam',
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-        metrics=['accuracy']
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"],
     )
-
-    model.build((None, 40, 30, 3))
 
     model.summary()
 
-    classes = [d for d in listdir(args.dataset) if not d.startswith('.')]
+    classes = [d for d in listdir(args.dataset) if not d.startswith(".")]
 
     # save classes to models/classes.txt
-    with open(args.classes, 'w') as f:
-        f.write('\n'.join(classes))
+    with open(args.classes, "w") as f:
+        f.write("\n".join(classes))
     # print summary to models/summary.txt
-    with open(args.summary, 'w') as f:
-        model.summary(print_fn=lambda x: f.write(x + '\n'))
+    with open(args.summary, "w") as f:
+        model.summary(print_fn=lambda x: f.write(x + "\n"))
 
     # Train model
     model.fit(train_data, epochs=args.epochs, validation_data=validation_data)
-    
+
     # Test model
     model.evaluate(train_data)
-    
+
     # Save model
     model.save(args.model)
-    
+
     # Test model
     model = keras.models.load_model(args.model)
-    model.evaluate(train_data)
-    
-    # Plot model
-    keras.utils.plot_model(model, to_file=args.plot, show_shapes=True, show_layer_names=True)
-    
+
+    keras.utils.plot_model(
+        model, to_file=args.plot, show_shapes=True, show_layer_names=True
+    )
+
+    predictions = model.predict(validation_data)
+    predictions = np.array([np.argmax(pred) for pred in predictions])
+
+    validation_labels = [labels.numpy() for _, labels in validation_data]
+
+    validation_labels = np.array([np.argmax(label) for label in validation_labels])
+
+    confusion_matrix = tf.math.confusion_matrix(validation_labels, predictions)
+
+    df_cm = pd.DataFrame(confusion_matrix, index=classes, columns=classes)
+    plt.figure(figsize=(6, 5))
+    sns.heatmap(df_cm, annot=True, fmt=".0f")
+    plt.savefig(args.confusion_matrix)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
